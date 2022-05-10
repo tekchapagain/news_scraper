3ea2362bd8bdd69c49c63df8d612ab28be2b06c2
tekchapagain <tekchapagain1@gmail.com>
2022-05-10 22:36:50 +0545

    main function updated

--- scrape.py
+++ scrape.py
@@ -1,77 +1,57 @@
 import requests
-import re
 import json
+import re
 
-search_term = "राजनीति"
-page_start, page_end =  1,4 # Each page has 10 articles
+
+search_title = "राजनीति"  #Change search Term here
+page_end = 3         #Change Page number here
 
 try:
-    with open(f"{search_term}.json") as f:
+    with open(f"{search_title}.json") as f:
         data = json.load(f)
 except:
-    data = {"page": 1}
-
+    data = {"page": 1, "items": []}
 
 def cleanhtml(raw_html):
     CLEANR = re.compile('<.*?>') 
     cleantext = re.sub(CLEANR, '', raw_html)
     return cleantext
 
-def load_article(search_term,page_start):
-    """Returns article list based on search_title"""
-
-    cleaned_articles = []  # list of articles
-
-    for page in range(
-        page_start, page_end
-    ):  # each page contains 10 articles in generally
-        url = (
-            "https://bg.annapurnapost.com/api/search?title="
-            + search_term
-            + "&page="
-            + str(page)
-        )
-        response = requests.get(url)
-        json_response = response.json()
-
-        try:
-            items = json_response["data"]["items"]
-            for i, _ in enumerate(items):  # loop over the articles
-                content = items[i]["content"]
-                cleaned_article = cleanhtml(content)
-                cleaned_articles.extend(
-                    cleaned_article
-                )  # append individual articles in a list
-            page_start +=1 # update the page index
-            print(f"Loading Page {page}")
-
-        except KeyError:
-            if cleaned_articles.__len__ == 0:
-                print("No articles found")
-                return None
-            else:
-                print("No more data availabel!!!")
-                return cleaned_articles
+def load_article():
+    url = (
+        "https://bg.annapurnapost.com/api/search?title="
+        + search_title
+        + "&page="
+        + str(page)
+    )
+    response = requests.get(url)
+    response = response.json()
+    cleaned_articles = []
+    try:
+        items = response["data"]["items"]
+        for i, _ in enumerate(items):  # loop over the articles
+            content = items[i]["content"]
+            cleaned_article = cleanhtml(content)
+            cleaned_articles.extend(
+                cleaned_article
+            )  # append individual articles in a list
+    except KeyError:
+        if cleaned_articles.__len__ == 0:
+            print("No articles found")
+            return None
+        else:
+            print("No more data availabel!!!")
+            return cleaned_articles
     return cleaned_articles
-
-
+        
 
 if __name__ == "__main__":
-    try:
-        current_page = page_start
-        with open(f"{search_term}.json","r+") as file:
-            previous_articles = json.load(file)
-            new_articles = load_article(search_term, current_page)
-            previous_articles.extend(
-                new_articles
-            )  # append previously loaded articles with newly fetched articles
-            json.dump(previous_articles, file)
-    except (KeyError, FileNotFoundError):
-        current_page = page_start
-        with open(f"{search_term}.json", "w") as file:
-            new_articles = load_article(search_term, current_page)
-            try:
-                json.dump(new_articles, file)
-            except:
-                print("Error occured while fetching!")
+    for page in range( data["page"], page_end): 
+        items = load_article()
+        data["items"].extend(items)
+        data["page"] += 1  # update the page index
+        print(f"Loading Page {page}")
+        with open(f"{search_title}.json", "w") as f:
+            json.dump(data, f)
 
+print("Completed!")
